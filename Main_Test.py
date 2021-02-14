7d97cd11b1a314e2c388dc79fe56521c1aa5d9f1
Vonkuro <vonkuro@gmail.com>
2021-02-14 15:29:33 +0100

    ajout de quelques fonctions de backend pour la BD

--- Main_Test.py
+++ Main_Test.py
@@ -1,7 +1,7 @@
 from Connexsion import *
 
 
-def verification_client(user, motsdepasse):
+def verification_client(user, motsdepasse): #testé
     base = connexion.cursor()
     base.execute("select * from Client;")
     for ligne in base :
@@ -12,7 +12,7 @@ def verification_client(user, motsdepasse):
     base.close()
     return 0
 
-def verification_admin(user, motsdepasse):
+def verification_admin(user, motsdepasse): #testé
     base = connexion.cursor()
     base.execute("select * from Administrateur;")
     for ligne in base :
@@ -23,7 +23,7 @@ def verification_admin(user, motsdepasse):
     base.close()
     return 0
 
-def ajout_client(user, motsdepasse, nom, prenom, datenaissance):
+def ajout_client(user, motsdepasse, nom, prenom, datenaissance): #testé
     base = connexion.cursor()
     base.execute("select top 1 IdPersonne from Personne order by IdPersonne Desc")
     ligne = base.fetchone()
@@ -39,7 +39,7 @@ def ajout_client(user, motsdepasse, nom, prenom, datenaissance):
 
     base.close()
 
-def ajout_admin(user, motsdepasse, nom, prenom, datenaissance):
+def ajout_admin(user, motsdepasse, nom, prenom, datenaissance): #testé
     base = connexion.cursor()
     base.execute("select top 1 IdPersonne from Personne order by IdPersonne Desc")
     ligne = base.fetchone()
@@ -52,17 +52,53 @@ def ajout_admin(user, motsdepasse, nom, prenom, datenaissance):
 
     base.close()
 
+def ajout_reservation(Client, Circuit, Places, Date): #testé
+    base = connexion.cursor()
+    base.execute("select top 1 IdReservation from Reservation order by IdReservation Desc")
+    ligne = base.fetchone()
+    Dernier_Id = ligne.IdReservation
+    Dernier_Id = Dernier_Id + 1 #str peut-être nécessaire
+    base.execute("INSERT INTO Reservation (IdReservation , Place , DateRevervation , Etat  , IdCircuit, IdPersonne) VALUES (?, ?, ? , '0', ? , ? ) ; ", [Dernier_Id, Places, Date, Circuit, Client])
+    base.execute("UPDATE Circuit SET NbPlaceDisponible= NbPlaceDisponible - ? where IdCircuit=? ;",[Places, Circuit])
+    base.close()
+
+def identification_passager(Nom, Prenom, DateNaissance): #testé
+    base = connexion.cursor()
+    base.execute("Select * from Personne;")
+    Id = 0
+    for ligne in base :
+        if Nom == ligne.Nom and Prenom == ligne.Prenom and DateNaissance == ligne.DateNaissance:
+            Id = ligne.IdPersonne
+    if Id == 0:
+        base.execute("select top 1 IdPersonne from Personne order by IdPersonne Desc")
+        ligne = base.fetchone()
+        Dernier_Id = ligne.IdPersonne + 1
+        base.execute("insert into Personne(IdPersonne, Nom, Prenom, DateNaissance) values (?, ?, ?, ?);", [Dernier_Id,Nom,Prenom,DateNaissance])
+        base.execute("insert into Passager(IdPersonne) values (?);",[Dernier_Id])
+        Id = Dernier_Id
+    else :
+        base.execute("Select * from Passager where IdPersonne = ?;",[Id])
+        ligne = base.fetchall()
+        if len(ligne)==0:
+            base.execute("insert into Passager(IdPersonne) values (?);",[Id])
+    base.close()
+    return Id
+
 
 def test_ajout():
-    ajout_admin("tard","tardy","Muliez","Marine", "2/8/1950")
+    trouve = identification_passager("Muliez","Pierre", "1950-08-28")
+    print(trouve)
+    trouve = identification_passager("Miliez","Michelle", "28/08/1965")
     base = connexion.cursor()
-    base.execute("select * from Administrateur, Personne where Administrateur.IdPersonne = Personne.IdPersonne")
+    print(trouve)
+    base.execute("select * from Passager, Personne where Passager.IdPersonne = Personne.IdPersonne;")
     for ligne in base:
         for i in range(len(ligne)):
             print(ligne[i], end=' ')
         print('')
     
     base.close()
+
 
 test_ajout()
 #connexion.commit()
